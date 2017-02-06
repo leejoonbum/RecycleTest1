3709635e3c27bd8812f76ba1f439d22d22040379
joonbumlee <joonmars@naver.com>
2017-02-06 11:02:03 +0900

    init2

--- app/src/main/java/taca/com/recycleviewtest/BaseActivity.java
+++ app/src/main/java/taca/com/recycleviewtest/BaseActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity
 {
+    // he
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
